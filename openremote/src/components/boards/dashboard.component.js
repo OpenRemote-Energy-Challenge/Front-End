62491b49a1a0bb13e636a7c8f79240b7914c5765
SP <devandebeer2000@gmail.com>
2021-04-21 15:52:20 +0200

    Added chart.js and added basic graph

--- openremote/src/components/boards/dashboard.component.js
+++ openremote/src/components/boards/dashboard.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import MainGraph from "./dashboard/powergraph.component";
 
 // Components
 import PrimaryGraph from './dashboard/primarygraph.component';
@@ -28,11 +29,62 @@ export default class DashboardComponent extends Component {
     }
 
     render() {
+        let currentDate = new Date();
+        let month = null;
+        let day = currentDate.getDay();
+        switch(currentDate.getMonth())
+        {
+            case 0: month = "January";
+                break;
+            case 1: month = "February";
+                break;
+            case 2: month = "March";
+                break;
+            case 3: month = "April"
+                break;
+            case 4: month = "May";
+                break;
+            case 5: month = "June";
+                break;
+            case 6: month = "July";
+                break;
+            case 7: month = "August";
+                break;
+            case 8: month = "September";
+                break;
+            case 9: month = "October";
+                break;
+            case 10: month = "November";
+                break;
+            case 11: month = "December";
+                break;
+        }
+        switch(currentDate.getDay())
+        {
+            case 0: day = "Sunday";
+                break;
+            case 1: day = "Monday";
+               break;
+            case 2: day = "Tuesday";
+                break;
+            case 3: day = "Wednesday";
+                break;
+            case 4: day = "Thursday";
+                break;
+            case 5: day = "Friday";
+                break;
+            case 6: day = "Saturday";
+                break;
+        }
+
         return (
             <div>
-                <h1>This is the dashboard</h1>
-                <PrimaryGraph />
+                <h1>Welcome back</h1>
+                <h3>Today is {day} {currentDate.getDate()} {month} {currentDate.getFullYear()}</h3>
+                <MainGraph/>
             </div>
         )
+
     }
+
 }
---
+++ openremote/src/components/boards/dashboard/powergraph.component.js
@@ -0,0 +1,49 @@
+import Chart from "chart.js/auto"
+import React from "react";
+
+var myChart;
+
+const MainGraph = () => {
+    let ctx = document.getElementById("maingraph");
+
+    if(myChart)
+    {
+        myChart.destroy();
+    }
+
+    myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Power Generated Today (kW)', 'Power Used Today (kW)', 'Estimated Time Left (H)'],
+            datasets: [{
+                label: '# of Votes',
+                data: [20, 10, 12],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    return(
+            <canvas id={"maingraph"}></canvas>
+    )
+
+}
+
+export default MainGraph;
