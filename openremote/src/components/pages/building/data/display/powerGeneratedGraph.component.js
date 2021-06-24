1eb573a510315800cf83a2e263726e060bbbd9eb
SP <devandebeer2000@gmail.com>
2021-06-24 14:24:07 +0200

    Added echarts js file

---
+++ openremote/src/components/pages/building/data/display/powerGeneratedGraph.component.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import ReactECharts from 'echarts-for-react';
+
+export default class PowerGeneratedGraphComponent extends Component{
+
+    render(){
+        const options = {
+            grid: { top: 8, right: 8, bottom: 24, left: 36 },
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: [
+                {
+                    data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    type: 'line',
+                    smooth: true,
+                },
+            ],
+            tooltip: {
+                trigger: 'axis',
+            },
+        };
+
+        return(
+            <ReactECharts option={options} />
+        )
+    }
+}
